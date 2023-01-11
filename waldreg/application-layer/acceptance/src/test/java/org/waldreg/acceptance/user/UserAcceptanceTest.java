1698124eaaeb44a303b2e3c70a49ea733afe3297
linirini <2001yerin@naver.com>
2023-01-12 05:28:27 +0900

    test : user 생성 실패 인수테스트 작성
    user 생성 실패 인수테스트 작성함

--- waldreg/application-layer/acceptance/src/test/java/org/waldreg/acceptance/user/UserAcceptanceTest.java
+++ waldreg/application-layer/acceptance/src/test/java/org/waldreg/acceptance/user/UserAcceptanceTest.java
@@ -1,15 +1,19 @@
 package org.waldreg.acceptance.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -19,21 +23,27 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 public class UserAcceptanceTest{
 
     @Autowired
-    MockMvc mvc;
+    private MockMvc mvc;
 
     @Autowired
-    ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
+
+    private final String apiVersion="1.0";
 
     @Test
     @DisplayName("유저 생성 성공 테스트")
     public void CREATE_NEW_USER_SUCCESS_TEST() throws Exception{
         //given
         String url = "/user";
+        String name = "alcuk";
+        String userId = "alcuk_id";
+        String userPassword="alcuk_pwd";
+        String phoneNumber="010-1234-1234";
         UserCreateRequest userCreateRequest = UserCreateRequest.builder()
-                .name("alcuk")
-                .userId("alcuk_id")
-                .userPassword("alcuk_pwd")
-                .phoneNumber("010-1234-1234")
+                .name(name)
+                .userId(userId)
+                .userPassword(userPassword)
+                .phoneNumber(phoneNumber)
                 .build();
 
         //when
@@ -41,17 +51,141 @@ public class UserAcceptanceTest{
                 .post(url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .header("api-version","1.0")
+                .header("api-version",apiVersion)
                 .content(objectMapper.writeValueAsString(userCreateRequest)));
 
         //then
         result.andExpectAll(
                 MockMvcResultMatchers.status().isOk(),
-                MockMvcResultMatchers.header().string("api-version","1.0")
+                MockMvcResultMatchers.header().string("api-version",apiVersion)
         ).andDo(MockMvcResultHandlers.print());
     }
 
-    private static class UserCreateRequest{
+
+    @Test
+    @DisplayName("유저 생성 실패 인수 테스트 - 중복 아이디")
+    public void CREATE_NEW_USER_FAIL_CAUSE_DUPLICATE_USER_ID() throws Exception{
+        //given
+        String url = "/user";
+        String name1 = "alcuk1";
+        String userId1 = "alcuk_id";
+        String userPassword1="alcuk_pwd1";
+        String phoneNumber1="010-1234-1111";
+        UserCreateRequest userCreateRequest1 = UserCreateRequest.builder()
+                .name(name1)
+                .userId(userId1)
+                .userPassword(userPassword1)
+                .phoneNumber(phoneNumber1)
+                .build();
+
+        String name2 = "alcuk2";
+        String userId2 = "alcuk_id";
+        String userPassword2="alcuk_pwd2";
+        String phoneNumber2="010-1234-2222";
+        UserCreateRequest userCreateRequest2 = UserCreateRequest.builder()
+                .name(name2)
+                .userId(userId2)
+                .userPassword(userPassword2)
+                .phoneNumber(phoneNumber2)
+                .build();
+
+        //when
+        mvc.perform(MockMvcRequestBuilders
+                .post(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("api-version",apiVersion)
+                .content(objectMapper.writeValueAsString(userCreateRequest1)));
+
+        ResultActions result=mvc.perform(MockMvcRequestBuilders
+                .post(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("api-version",apiVersion)
+                .content(objectMapper.writeValueAsString(userCreateRequest2)));
+
+        //then
+        result.andExpectAll(
+                MockMvcResultMatchers.status().isBadRequest(),
+                MockMvcResultMatchers.header().string(HttpHeaders.CONTENT_TYPE,"application/json"),
+                MockMvcResultMatchers.header().string("api-version",apiVersion),
+                MockMvcResultMatchers.jsonPath("$.messages").value("Duplicated user_id"),
+                MockMvcResultMatchers.jsonPath("$.document_url").value("docs.waldreg.org")
+        ).andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    @DisplayName("유저 생성 실패 인수 테스트 - 비밀번호 보안 기준 미달")
+    public void CREATE_NEW_USER_FAIL_CAUSE_UNSECURE_USER_PWD() throws Exception{
+        //given
+        String url = "/user";
+        String name = "alcuk";
+        String userId = "alcuk_id";
+        String userPassword="";
+        String phoneNumber="010-1234-1234";
+        UserCreateRequest userCreateRequest = UserCreateRequest.builder()
+                .name(name)
+                .userId(userId)
+                .userPassword(userPassword)
+                .phoneNumber(phoneNumber)
+                .build();
+
+        //when
+        ResultActions result = mvc.perform(MockMvcRequestBuilders
+                .post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("api-version",apiVersion)
+                .content(objectMapper.writeValueAsString(userCreateRequest)));
+
+        //then
+        result.andExpectAll(
+                MockMvcResultMatchers.status().isBadRequest(),
+                MockMvcResultMatchers.header().string(HttpHeaders.CONTENT_TYPE,"application/json"),
+                MockMvcResultMatchers.header().string("api-version",apiVersion),
+                MockMvcResultMatchers.jsonPath("$.messages").value("Unsecured user_password"),
+                MockMvcResultMatchers.jsonPath("$.document_url").value("docs.waldreg.org")
+        ).andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    @DisplayName("유저 생성 실패 인수 테스트 - 비밀번호 보안 기준 미달")
+    public void CREATE_NEW_USER_FAIL_CAUSE_INVALID_REQUEST() throws Exception{
+        //given
+        String url = "/user";
+        String name = "";
+        String userId = "";
+        String userPassword="alcuk_pwd";
+        String phoneNumber="010-1234-1234";
+        UserCreateRequest userCreateRequest = UserCreateRequest.builder()
+                .name(name)
+                .userId(userId)
+                .userPassword(userPassword)
+                .phoneNumber(phoneNumber)
+                .build();
+
+        //when
+        ResultActions result=mvc.perform(MockMvcRequestBuilders
+                .post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("api-version",apiVersion)
+                .content(objectMapper.writeValueAsString(userCreateRequest)));
+
+        //then
+        result.andExpectAll(
+                MockMvcResultMatchers.status().isBadRequest(),
+                MockMvcResultMatchers.header().string(HttpHeaders.CONTENT_TYPE,"application/json"),
+                MockMvcResultMatchers.header().string("api-version",apiVersion),
+                MockMvcResultMatchers.jsonPath("$.messages").value("Invalid request"),
+                MockMvcResultMatchers.jsonPath("$.document_url").value("docs.waldreg.org")
+        ).andDo(MockMvcResultHandlers.print());
+
+    }
+
+    private final static class UserCreateRequest{
 
         @JsonProperty("name")
         private String name;
@@ -81,7 +215,15 @@ public class UserAcceptanceTest{
 
         public String getPhoneNumber(){return phoneNumber;}
 
-        public static class Builder{
+        public void setName(String name){this.name = name;}
+
+        public void setUserId(String userId){this.userId = userId;}
+
+        public void setUserPassword(String userPassword){this.userPassword = userPassword;}
+
+        public void setPhoneNumber(String phoneNumber){this.phoneNumber = phoneNumber;}
+
+        public final static class Builder{
 
             private String name;
             private String userId;
