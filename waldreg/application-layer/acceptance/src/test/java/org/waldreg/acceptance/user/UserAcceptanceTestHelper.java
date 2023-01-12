7ed8f6aaf19f6034f32f6a957fe41e0d72339fdc
linirini <2001yerin@naver.com>
2023-01-12 12:37:36 +0900

    test : 유저 수정 인수 테스트
    유저 수정 인수 테스트 작성 및 인수테스트 리펙토링

---
+++ waldreg/application-layer/acceptance/src/test/java/org/waldreg/acceptance/user/UserAcceptanceTestHelper.java
@@ -0,0 +1,69 @@
+package org.waldreg.acceptance.user;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+public class UserAcceptanceTestHelper{
+
+    private final static String apiVersion = "1.0";
+
+    public static ResultActions createUser(MockMvc mvc, String url, String content)
+            throws Exception{
+        return mvc.perform(MockMvcRequestBuilders
+                .post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("api-version", apiVersion)
+                .content(content));
+    }
+
+    public static ResultActions inquiryUserWithToken(MockMvc mvc, String url, String name, String token)
+            throws Exception{
+        return mvc.perform(MockMvcRequestBuilders
+                .get(url, name)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("api-version", apiVersion)
+                .header(HttpHeaders.AUTHORIZATION, token));
+    }
+
+    public static ResultActions inquiryUserWithoutToken(MockMvc mvc, String url, String name)
+            throws Exception{
+        return mvc.perform(MockMvcRequestBuilders
+                .get(url, name)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("api-version", apiVersion));
+    }
+
+    public static ResultActions inquiryUserOnline(MockMvc mvc, String url, String token)
+            throws Exception{
+        return mvc.perform(MockMvcRequestBuilders
+                .get(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("api-version", apiVersion)
+                .header(HttpHeaders.AUTHORIZATION, token));
+    }
+
+    public static ResultActions inquiryUserOnlineWithNoToken(MockMvc mvc, String url)
+            throws Exception{
+        return mvc.perform(MockMvcRequestBuilders
+                .get(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("api-version", apiVersion));
+    }
+
+    public static ResultActions modifyUser(MockMvc mvc, String url, String token, String pwd, String content)throws Exception{
+        return mvc.perform(MockMvcRequestBuilders
+                .patch(url, content)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("api-version", apiVersion)
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .header("password",pwd)
+                .content(content));
+    }
+
+
+
+}
