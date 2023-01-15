7c77f5592863f950d5e9c1cada26a76a94211a9b
seongha <11pi885@gmail.com>
2023-01-15 18:15:08 +0900

    feat : jwt 인증시 정보 저장 추가
    DecryptedTokenContextHolder에 인증 정보 추가

--- waldreg/service-layer/auth/src/main/java/org/waldreg/token/jwt/authenticator/JwtAuthenticator.java
+++ waldreg/service-layer/auth/src/main/java/org/waldreg/token/jwt/authenticator/JwtAuthenticator.java
@@ -1,6 +1,8 @@
 package org.waldreg.token.jwt.authenticator;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,22 +10,27 @@ import org.springframework.stereotype.Service;
 import org.waldreg.token.authenticator.TokenAuthenticator;
 import org.waldreg.token.exception.TokenExpiredException;
 import org.waldreg.token.jwt.secret.Secret;
+import org.waldreg.util.DecryptedTokenContextHolder;
 
 @Service
 public class JwtAuthenticator implements TokenAuthenticator{
 
     private final Secret secret;
 
+    private DecryptedTokenContextHolder decryptedTokenContextHolder;
     @Autowired
-    public JwtAuthenticator(Secret secret){
+    public JwtAuthenticator(Secret secret, DecryptedTokenContextHolder decryptedTokenContextHolder){
         this.secret = secret;
+        this.decryptedTokenContextHolder = decryptedTokenContextHolder;
     }
 
     @Override
     public boolean authenticate(String token) throws JwtException{
         try{
-            Jwts.parserBuilder().setSigningKey(secret.getSecretKey()).build().parseClaimsJws(token);
-
+            Jws<Claims> claim = Jwts.parserBuilder().setSigningKey(secret.getSecretKey()).build().parseClaimsJws(token);
+            System.out.println("\n\n" + claim.getBody().getSubject());
+            int id = Integer.parseInt(claim.getBody().getSubject());
+            decryptedTokenContextHolder.hold(id);
             return true;
         } catch (ExpiredJwtException EJE){
             throw new TokenExpiredException(EJE.getMessage(), EJE.getCause());
