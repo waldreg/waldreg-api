7bada4ee66679d72242757c7c5e858b41298a7cb
devxb <devxb@naver.com>
2023-01-16 14:27:15 +0900

    feat : Application 시작시 "Admin" 역할이 없다면 추가하도록 설정
    - Application 시작시 "Admin" 역할이 비어있다면, 모든 권한에 대해 성공하는 "Admin" 역할을 기본으로 추가하도록 설정함.

---
+++ waldreg/application-layer/application-configurer/src/test/java/org/waldreg/ConfigurerTestInitializer.java
@@ -0,0 +1,13 @@
+package org.waldreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurerTestInitializer{
+
+    public static void main(String[] args){
+        SpringApplication.run(ConfigurerTestInitializer.class, args);
+    }
+
+}
---
+++ waldreg/application-layer/application-configurer/src/test/java/org/waldreg/config/character/AdminCharacterConfigurerTest.java
@@ -0,0 +1,46 @@
+package org.waldreg.config.character;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.waldreg.character.dto.CharacterDto;
+import org.waldreg.character.dto.PermissionDto;
+import org.waldreg.character.management.CharacterManager;
+import org.waldreg.character.permission.verification.PermissionVerifier;
+
+@SpringBootTest
+public class AdminCharacterConfigurerTest{
+
+    @Autowired
+    private CharacterManager characterManager;
+
+    @Autowired
+    private PermissionVerifier permissionVerifier;
+
+    @Test
+    @DisplayName("애플리케이션 시작시 \"Admin\" character 추가 확인 테스트")
+    public void ADMIN_CHARACTER_SET_UP_TEST(){
+        // given
+        String adminName = "Admin";
+
+        // when
+        CharacterDto characterDto = characterManager.readCharacter(adminName);
+
+        // then
+        Assertions.assertAll(
+                ()-> Assertions.assertEquals(adminName, characterDto.getCharacterName()),
+                ()-> Assertions.assertTrue(isAllPermissionVerified(characterDto.getPermissionList()))
+        );
+    }
+
+    private boolean isAllPermissionVerified(List<PermissionDto> permissionDtoList){
+        for(PermissionDto permissionDto : permissionDtoList){
+            if(!permissionVerifier.verify(permissionDto.getName(), permissionDto.getStatus())) return false;
+        }
+        return true;
+    }
+
+}
