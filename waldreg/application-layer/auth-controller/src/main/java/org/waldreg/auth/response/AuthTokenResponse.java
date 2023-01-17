fca14946d5da783ec185d75423e141cb3de77377
seongha <11pi885@gmail.com>
2023-01-17 12:51:47 +0900

    feat : auth-controller 추가

---
+++ waldreg/application-layer/auth-controller/src/main/java/org/waldreg/auth/response/AuthTokenResponse.java
@@ -0,0 +1,54 @@
+package org.waldreg.auth.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AuthTokenResponse{
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    private AuthTokenResponse(Builder builder){
+        this.accessToken = builder.accessToken;
+        this.tokenType = builder.tokenType;
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public String getAccessToken(){
+        return accessToken;
+    }
+
+    public String getTokenType(){
+        return tokenType;
+    }
+
+    public static final class Builder{
+
+        private String accessToken;
+
+        private String tokenType;
+
+        private Builder(){}
+
+        public Builder accessToken(String accessToken){
+            this.accessToken = accessToken;
+            return this;
+        }
+
+        public Builder tokenType(String tokenType){
+            this.tokenType = tokenType;
+            return this;
+        }
+
+        public AuthTokenResponse build(){
+            return new AuthTokenResponse(this);
+        }
+
+    }
+
+}
