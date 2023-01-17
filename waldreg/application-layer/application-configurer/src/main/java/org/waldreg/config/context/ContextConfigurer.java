40ce7eec54fee7f62c04a0e5e1d5f4e894d2bbf8
devxb <devxb@naver.com>
2023-01-17 13:39:19 +0900

    feat : DecryptedContextResolverInterceptor 기본 등록 기능 추가
    application 시작시 DecryptedContextResolverInterceptor 를 기본 인터셉터로 등록함.

---
+++ waldreg/application-layer/application-configurer/src/main/java/org/waldreg/config/context/ContextConfigurer.java
@@ -0,0 +1,24 @@
+package org.waldreg.config.context;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ContextConfigurer implements WebMvcConfigurer{
+
+    private final DecryptedContextResolveInterceptor decryptedContextResolveInterceptor;
+
+    public ContextConfigurer(DecryptedContextResolveInterceptor decryptedContextResolveInterceptor){
+        this.decryptedContextResolveInterceptor = decryptedContextResolveInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(decryptedContextResolveInterceptor)
+                .addPathPatterns("**");
+    }
+
+
+}
--- waldreg/application-layer/application-configurer/src/main/java/org/waldreg/config/context/DecryptedContextResolveInterceptor.java
+++ waldreg/application-layer/application-configurer/src/main/java/org/waldreg/config/context/DecryptedContextResolveInterceptor.java
@@ -3,9 +3,11 @@ package org.waldreg.config.context;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.waldreg.util.token.DecryptedTokenContextResolver;
 
+@Component
 public class DecryptedContextResolveInterceptor implements HandlerInterceptor{
 
     private final DecryptedTokenContextResolver decryptedTokenContextResolver;
