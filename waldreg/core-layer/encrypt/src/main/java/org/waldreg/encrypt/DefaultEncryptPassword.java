8a94188bb2412217319168d342c6fdf22b5eabaf
seongha <11pi885@gmail.com>
2023-01-23 14:36:08 +0900

    feat : 비밀번호 암호화 기능 추가
    sha-256 알고리즘을 사용한 비밀번호 암호화 기능 추가

---
+++ waldreg/core-layer/encrypt/src/main/java/org/waldreg/encrypt/DefaultEncryptPassword.java
@@ -0,0 +1,45 @@
+package org.waldreg.encrypt;
+
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class DefaultEncryptPassword implements EncryptPassword{
+
+    @Override
+    public String createSalt(){
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[20];
+
+        secureRandom.nextBytes(salt);
+
+        StringBuffer buffer = new StringBuffer();
+        for (byte b : salt){
+            buffer.append(String.format("%02x", b));
+        }
+
+        return buffer.toString();
+    }
+
+    @Override
+    public String getEncryptPassword(String password, String salt){
+        String result;
+        try{
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update((password + salt).getBytes());
+            byte[] passwordSalt = messageDigest.digest();
+
+            StringBuffer buffer = new StringBuffer();
+            for (byte b : passwordSalt){
+                buffer.append(String.format("%02x", b));
+            }
+            result = buffer.toString();
+
+        } catch (NoSuchAlgorithmException NSAE){
+            throw new RuntimeException(NSAE);
+        }
+        return result;
+    }
+
+}
