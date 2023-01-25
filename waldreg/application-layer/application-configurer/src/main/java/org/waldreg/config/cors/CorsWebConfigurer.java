7de3d16662a517d11fd6d31a440144188e761770
devxb <devxb@naver.com>
2023-01-25 14:00:13 +0900

    fix : CORS 설정 추가
    CORS가 설정을 추가했습니다.

--- waldreg/application-layer/application-configurer/src/main/java/org/waldreg/config/cors/CorsWebConfigurer.java
+++ waldreg/application-layer/application-configurer/src/main/java/org/waldreg/config/cors/CorsWebConfigurer.java
@@ -1,8 +1,10 @@
 package org.waldreg.config.cors;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsWebConfigurer implements WebMvcConfigurer{
 
     @Override
