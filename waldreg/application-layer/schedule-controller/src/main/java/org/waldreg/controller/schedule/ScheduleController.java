f4c05b866a88853ebf37e83fef741615eff24c59
linirini <2001yerin@naver.com>
2023-01-28 19:20:19 +0900

    feat : read Specific Schedule Controller
    - code for reading specific Schedule Controller

--- waldreg/application-layer/schedule-controller/src/main/java/org/waldreg/controller/schedule/ScheduleController.java
+++ waldreg/application-layer/schedule-controller/src/main/java/org/waldreg/controller/schedule/ScheduleController.java
@@ -2,6 +2,8 @@ package org.waldreg.controller.schedule;
 
 import java.util.List;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +37,12 @@ public class ScheduleController{
     public List<ScheduleResponse> readScheduleByTerm(@RequestParam("year") int year, @RequestParam("month") int month){
         List<ScheduleDto> scheduleDtoList = scheduleManager.readScheduleByTerm(year, month);
         return controllerScheduleMapper.scheduleDtoListToScheduleResponseList(scheduleDtoList);
+    }
+
+    @GetMapping("/schedule/{schedule-id}")
+    public ScheduleResponse readScheduleById(@PathVariable("schedule-id")int id){
+        ScheduleDto scheduleDto = scheduleManager.readScheduleById(id);
+        return controllerScheduleMapper.scheduleDtoToScheduleResponse(scheduleDto);
     }
 
 }
