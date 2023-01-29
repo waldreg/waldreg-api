77fc4ebecbc22eb237c47aa9eaf70d2cdf395aa1
devxb <devxb@naver.com>
2023-01-29 16:22:57 +0900

    feat : reward tag목록을 조회하는 컨트롤러 추가
    - 생성된 reward tag 목록을 조회하는 컨트롤러를 추가했습니다

--- waldreg/application-layer/acceptance/src/test/java/org/waldreg/acceptance/reward/RewardAcceptanceTest.java
+++ waldreg/application-layer/acceptance/src/test/java/org/waldreg/acceptance/reward/RewardAcceptanceTest.java
@@ -24,6 +24,7 @@ import org.waldreg.acceptance.authentication.AuthenticationAcceptanceTestHelper;
 import org.waldreg.acceptance.user.UserAcceptanceTestHelper;
 import org.waldreg.auth.request.AuthTokenRequest;
 import org.waldreg.controller.reward.request.RewardTagRequest;
+import org.waldreg.controller.reward.response.RewardTagResponse;
 import org.waldreg.controller.user.request.UserRequest;
 import org.waldreg.controller.user.response.UserResponse;
 
@@ -917,69 +918,6 @@ public class RewardAcceptanceTest{
     private UsersRewardTagResponse getUsersRewardTagResponse(ResultActions resultActions) throws Exception{
         String content = resultActions.andReturn().getResponse().getContentAsString();
         return objectMapper.readValue(content, UsersRewardTagResponse.class);
-    }
-
-    public final static class RewardTagResponse{
-
-        private final int rewardTagId;
-        private final String rewardTagTitle;
-        private final int rewardPoint;
-
-        private RewardTagResponse(){
-            throw new UnsupportedOperationException("Can not invoke constructor \"RewardTagResponse()\"");
-        }
-
-        private RewardTagResponse(Builder builder){
-            this.rewardTagId = builder.rewardTagId;
-            this.rewardTagTitle = builder.rewardTagTitle;
-            this.rewardPoint = builder.rewardPoint;
-        }
-
-        public static Builder builder(){
-            return new Builder();
-        }
-
-        public int getRewardTagId(){
-            return rewardTagId;
-        }
-
-        public String getRewardTagTitle(){
-            return rewardTagTitle;
-        }
-
-        public int getRewardPoint(){
-            return rewardPoint;
-        }
-
-        public final static class Builder{
-
-            private int rewardTagId;
-            private String rewardTagTitle;
-            private int rewardPoint;
-
-            private Builder(){}
-
-            public Builder rewardTagId(int rewardTagId){
-                this.rewardTagId = rewardTagId;
-                return this;
-            }
-
-            public Builder rewardTagTitle(String rewardTagTitle){
-                this.rewardTagTitle = rewardTagTitle;
-                return this;
-            }
-
-            public Builder rewardPoint(int rewardPoint){
-                this.rewardPoint = rewardPoint;
-                return this;
-            }
-
-            public RewardTagResponse build(){
-                return new RewardTagResponse(this);
-            }
-
-        }
-
     }
 
     public final static class UsersRewardTagResponse{
--- waldreg/application-layer/reward-controller/src/main/java/org/waldreg/controller/reward/RewardController.java
+++ waldreg/application-layer/reward-controller/src/main/java/org/waldreg/controller/reward/RewardController.java
@@ -1,14 +1,21 @@
 package org.waldreg.controller.reward;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.wadlreg.reward.tag.RewardTagManager;
+import org.wadlreg.reward.tag.dto.RewardTagDto;
 import org.wadlreg.reward.users.UsersRewardManager;
+import org.waldreg.character.aop.annotation.PermissionVerifying;
 import org.waldreg.controller.reward.mapper.ControllerRewardTagMapper;
 import org.waldreg.controller.reward.request.RewardTagRequest;
+import org.waldreg.controller.reward.response.RewardTagResponse;
+import org.waldreg.token.aop.annotation.Authenticating;
 
 @RestController
 public class RewardController{
@@ -26,11 +33,21 @@ public class RewardController{
         this.controllerRewardTagMapper = controllerRewardTagMapper;
     }
 
+    @Authenticating
+    @PermissionVerifying("Reward manager")
     @PostMapping("/reward-tag")
     public void createRewardTag(@RequestBody @Validated RewardTagRequest rewardTagRequest){
         rewardTagManager.createRewardTag(
                 controllerRewardTagMapper.rewardTagRequestToRewardTagDto(rewardTagRequest)
         );
+    }
+
+    @Authenticating
+    @PermissionVerifying("Reward manager")
+    @GetMapping("/reward-tag")
+    public Map<String, List<RewardTagResponse>> readRewardTagMap(){
+        List<RewardTagDto> rewardTagDtoList = rewardTagManager.readRewardTagList();
+        return Map.of("reward_tags", controllerRewardTagMapper.rewardTagDtoListToRewardTagResponseList(rewardTagDtoList));
     }
 
 }
