152f8d4e3449df54ba43799c8ce900646af424bb
devxb <devxb@naver.com>
2023-01-30 13:19:00 +0900

    fix : Cors 설정 수정
    - EnableWebMvc 어노테이션 추가

--- waldreg/application-layer/application-configurer/src/main/java/org/waldreg/config/cors/CorsWebConfigurer.java
+++ waldreg/application-layer/application-configurer/src/main/java/org/waldreg/config/cors/CorsWebConfigurer.java
@@ -2,9 +2,11 @@ package org.waldreg.config.cors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsWebConfigurer implements WebMvcConfigurer{
 
     @Override
