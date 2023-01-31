7adec79cc2fdd72db0e886919854df0e35778004
linirini <2001yerin@naver.com>
2023-01-31 16:08:07 +0900

    feat : InvalidDateFormatException Handler
    - InvalidDateFormatException Handler 추가

--- waldreg/application-layer/schedule-controller/src/main/java/org/waldreg/controller/schedule/exceptionadvice/ScheduleControllerAdvice.java
+++ waldreg/application-layer/schedule-controller/src/main/java/org/waldreg/controller/schedule/exceptionadvice/ScheduleControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.waldreg.schedule.exception.ContentOverflowException;
+import org.waldreg.schedule.exception.InvalidDateFormatException;
 
 @RestControllerAdvice
 public class ScheduleControllerAdvice{
@@ -15,6 +16,15 @@ public class ScheduleControllerAdvice{
     public ResponseEntity<ExceptionTemplate> catchContentOverflowException(ContentOverflowException contentOverflowException){
         ExceptionTemplate exceptionTemplate = ExceptionTemplate.builder()
                 .message(contentOverflowException.getMessage())
+                .documentUrl(documentUrl)
+                .build();
+        return new ResponseEntity<>(exceptionTemplate, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({InvalidDateFormatException.class})
+    public ResponseEntity<ExceptionTemplate> catchInvalidDateFormatException(InvalidDateFormatException invalidDateFormatException){
+        ExceptionTemplate exceptionTemplate = ExceptionTemplate.builder()
+                .message(invalidDateFormatException.getMessage())
                 .documentUrl(documentUrl)
                 .build();
         return new ResponseEntity<>(exceptionTemplate, HttpStatus.BAD_REQUEST);
