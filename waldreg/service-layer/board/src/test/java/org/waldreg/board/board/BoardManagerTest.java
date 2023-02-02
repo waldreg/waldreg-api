1fbe59201923bc4780e5e5d62e1acb779a2c2109
seongha <11pi885@gmail.com>
2023-02-02 11:17:00 +0900

    test : board 조회 실패 테스트 추가

--- waldreg/service-layer/board/src/test/java/org/waldreg/board/board/BoardManagerTest.java
+++ waldreg/service-layer/board/src/test/java/org/waldreg/board/board/BoardManagerTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.waldreg.board.board.exception.BoardDoesNotExistException;
 import org.waldreg.board.board.management.BoardManager;
 import org.waldreg.board.board.management.BoardManager.BoardRequest;
 import org.waldreg.board.board.management.DefaultBoardManager;
@@ -101,14 +101,50 @@ public class BoardManagerTest{
         BoardDto result = boardManager.inquiryBoardById(1);
         //then
         Assertions.assertAll(
-                () -> Assertions.assertEquals(boardDto.getId(),result.getId()),
-                () -> Assertions.assertEquals(boardDto.getTitle(),result.getTitle()),
-                () -> Assertions.assertEquals(boardDto.getContent(),result.getContent()),
-                () -> Assertions.assertEquals(boardDto.getCategory(),result.getCategory()),
-                () -> Assertions.assertEquals(boardDto.getMemberTier(),result.getMemberTier()),
-                () -> Assertions.assertEquals(boardDto.getUser(),result.getUser())
+                () -> Assertions.assertEquals(boardDto.getId(), result.getId()),
+                () -> Assertions.assertEquals(boardDto.getTitle(), result.getTitle()),
+                () -> Assertions.assertEquals(boardDto.getContent(), result.getContent()),
+                () -> Assertions.assertEquals(boardDto.getCategory(), result.getCategory()),
+                () -> Assertions.assertEquals(boardDto.getMemberTier(), result.getMemberTier()),
+                () -> Assertions.assertEquals(boardDto.getUser(), result.getUser())
         );
 
     }
+
+    @Test
+    @DisplayName("아이디로 게시글 조회 실패 테스트")
+    public void INQUIRY_BOARD_BY_ID_FAIL_TEST(){
+        //given
+        String title = "title";
+        String content = "content";
+
+        int id = 1;
+        int categoryId = 1;
+        MemberTier memberTier = MemberTier.TIER_3;
+
+        UserDto userDto = UserDto.builder()
+                .id(id)
+                .build();
+        CategoryDto categoryDto = CategoryDto.builder()
+                .id(categoryId)
+                .categoryName("categoryName")
+                .build();
+
+        BoardDto boardDto = BoardDto.builder()
+                .id(1)
+                .user(userDto)
+                .category(categoryDto)
+                .memberTier(memberTier)
+                .title(title)
+                .content(content)
+                .build();
+
+        Mockito.when(boardRepository.isExistBoard(Mockito.anyInt())).thenReturn(false);
+        Mockito.when(boardRepository.inquiryBoardById(Mockito.anyInt())).thenReturn(boardDto);
+
+        //when&then
+        Assertions.assertThrows(BoardDoesNotExistException.class, () -> boardManager.inquiryBoardById(1));
+    }
+
 
 }
