1daaa0676ce97c43acc4dcc70ee45ce366e47552
devxb <devxb@naver.com>
2023-02-20 16:23:45 +0900

    refactor : 검증관련 로직을 repository -> service layer로 옮김
    - 검증 관련 로직을 repository-layer에서 service-layer로 옮겼습니다.

--- waldreg/repository-layer/memory-repository/src/main/java/org/waldreg/repository/character/MemoryCharacterRepository.java
+++ waldreg/repository-layer/memory-repository/src/main/java/org/waldreg/repository/character/MemoryCharacterRepository.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.waldreg.character.dto.CharacterDto;
-import org.waldreg.character.exception.DuplicatedCharacterException;
-import org.waldreg.character.exception.UnknownCharacterException;
 import org.waldreg.character.spi.CharacterRepository;
 import org.waldreg.domain.character.Character;
 import org.waldreg.domain.character.Permission;
@@ -35,7 +34,6 @@ public class MemoryCharacterRepository implements CharacterRepository{
 
     @Override
     public synchronized void createCharacter(CharacterDto characterDto){
-        throwIfDuplicatedCharacterNameDetected(characterDto);
         memoryCharacterStorage.createCharacter(
                 characterMapper.characterDtoToDomain(characterDto)
         );
@@ -43,10 +41,6 @@ public class MemoryCharacterRepository implements CharacterRepository{
 
     @Override
     public void updateCharacter(String targetName, CharacterDto changedCharacter){
-        throwIfCharacterDoesNotExist(targetName);
-        if(!targetName.equals(changedCharacter.getCharacterName())){
-            throwIfDuplicatedCharacterNameDetected(changedCharacter);
-        }
         Character beforeCharacter = memoryCharacterStorage.readCharacterByName(targetName);
         List<Permission> changedPermissionList = getChangedPermissionList(beforeCharacter.getPermissionList(),
                 characterMapper.characterDtoToDomain(changedCharacter).getPermissionList());
@@ -55,12 +49,6 @@ public class MemoryCharacterRepository implements CharacterRepository{
                 .characterName(changedCharacter.getCharacterName())
                 .permissionList(changedPermissionList)
                 .build());
-    }
-
-    private void throwIfDuplicatedCharacterNameDetected(CharacterDto characterDto){
-        if (memoryCharacterStorage.readCharacterByName(characterDto.getCharacterName()) != null){
-            throw new DuplicatedCharacterException(characterDto.getCharacterName());
-        }
     }
 
     private List<Permission> getChangedPermissionList(List<Permission> beforePermissionList, List<Permission> afterPermissionList){
@@ -88,29 +76,23 @@ public class MemoryCharacterRepository implements CharacterRepository{
     }
 
     @Override
-    public CharacterDto readCharacter(String characterName){
-        throwIfCharacterDoesNotExist(characterName);
+    public Optional<CharacterDto> readCharacter(String characterName){
         Character character = memoryCharacterStorage.readCharacterByName(characterName);
-        return characterMapper.characterDomainToDto(character);
+        if(character == null){
+            return Optional.empty();
+        }
+        return Optional.of(characterMapper.characterDomainToDto(character));
     }
 
     @Override
     public CharacterDto readCharacterByUserId(int id){
         User user = memoryUserStorage.readUserById(id);
-        throwIfCharacterDoesNotExist(user.getCharacter().getCharacterName());
         return characterMapper.characterDomainToDto(user.getCharacter());
     }
 
     @Override
     public void deleteCharacter(String characterName){
-        throwIfCharacterDoesNotExist(characterName);
         memoryCharacterStorage.deleteCharacterByName(characterName);
-    }
-
-    private void throwIfCharacterDoesNotExist(String characterName){
-        if (memoryCharacterStorage.readCharacterByName(characterName) == null){
-            throw new UnknownCharacterException(characterName);
-        }
     }
 
     @Override
