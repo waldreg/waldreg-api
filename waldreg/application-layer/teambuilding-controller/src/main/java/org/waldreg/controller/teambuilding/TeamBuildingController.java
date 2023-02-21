b3c2958fc29ebbd6d460cc2107bba849fc47c4d1
linirini <2001yerin@naver.com>
2023-02-21 19:24:49 +0900

    feat : PostMappingController 추가 : 팀 추가

--- waldreg/application-layer/teambuilding-controller/src/main/java/org/waldreg/controller/teambuilding/TeamBuildingController.java
+++ waldreg/application-layer/teambuilding-controller/src/main/java/org/waldreg/controller/teambuilding/TeamBuildingController.java
@@ -1,12 +1,16 @@
 package org.waldreg.controller.teambuilding;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.waldreg.character.aop.annotation.PermissionVerifying;
 import org.waldreg.controller.teambuilding.mapper.ControllerTeamBuildingMapper;
 import org.waldreg.controller.teambuilding.request.TeamBuildingRequest;
+import org.waldreg.controller.teambuilding.request.TeamRequest;
+import org.waldreg.teambuilding.team.dto.TeamRequestDto;
+import org.waldreg.teambuilding.team.management.TeamManager;
 import org.waldreg.teambuilding.teambuilding.dto.TeamBuildingRequestDto;
 import org.waldreg.teambuilding.teambuilding.management.TeamBuildingManager;
 import org.waldreg.token.aop.annotation.Authenticating;
@@ -16,11 +20,14 @@ public class TeamBuildingController{
 
     private final TeamBuildingManager teamBuildingManager;
 
+    private final TeamManager teamManager;
+
     private final ControllerTeamBuildingMapper controllerTeamBuildingMapper;
 
 
-    public TeamBuildingController(TeamBuildingManager teamBuildingManager, ControllerTeamBuildingMapper controllerTeamBuildingMapper){
+    public TeamBuildingController(TeamBuildingManager teamBuildingManager, TeamManager teamManager, ControllerTeamBuildingMapper controllerTeamBuildingMapper){
         this.teamBuildingManager = teamBuildingManager;
+        this.teamManager = teamManager;
         this.controllerTeamBuildingMapper = controllerTeamBuildingMapper;
     }
 
@@ -30,6 +37,14 @@ public class TeamBuildingController{
     public void createTeamBuilding(@RequestBody @Validated TeamBuildingRequest teamBuildingRequest){
         TeamBuildingRequestDto teamBuildingRequestDto = controllerTeamBuildingMapper.teamBuildingRequestToTeamBuildingDto(teamBuildingRequest);
         teamBuildingManager.createTeamBuilding(teamBuildingRequestDto);
+    }
+
+    @Authenticating
+    @PermissionVerifying(value = "Teambuilding create manager")
+    @PostMapping("/teambuilding/{teambuilding-id}")
+    public void createTeam(@PathVariable("teambuilding-id") int teamBuildingId, @RequestBody @Validated TeamRequest teamRequest){
+        TeamRequestDto teamRequestDto = controllerTeamBuildingMapper.teamRequestToTeamRequestDto(teamRequest);
+        teamManager.createTeam(teamBuildingId, teamRequestDto);
     }
 
 
