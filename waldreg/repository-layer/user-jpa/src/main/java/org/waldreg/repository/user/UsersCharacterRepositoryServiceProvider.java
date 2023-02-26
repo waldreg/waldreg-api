428ef8a5f8500e57071caa7ad0e66f45944a9983
linirini <2001yerin@naver.com>
2023-02-26 14:47:05 +0900

    test : JpaCharacterRepository & UsersCharacterRepositoryServiceProvider Test

--- waldreg/repository-layer/user-jpa/src/main/java/org/waldreg/repository/user/UsersCharacterRepositoryServiceProvider.java
+++ waldreg/repository-layer/user-jpa/src/main/java/org/waldreg/repository/user/UsersCharacterRepositoryServiceProvider.java
@@ -1,7 +1,7 @@
 package org.waldreg.repository.user;
 
 import org.springframework.stereotype.Repository;
-import org.waldreg.domain.character.Character;
+import org.springframework.transaction.annotation.Transactional;
 import org.waldreg.repository.user.repository.JpaCharacterRepository;
 import org.waldreg.user.spi.UsersCharacterRepository;
 
@@ -13,12 +13,9 @@ public class UsersCharacterRepositoryServiceProvider implements UsersCharacterRe
     public UsersCharacterRepositoryServiceProvider(JpaCharacterRepository jpaCharacterRepository){this.jpaCharacterRepository = jpaCharacterRepository;}
 
     @Override
+    @Transactional(readOnly = true)
     public boolean isExistCharacterName(String characterName){
         return jpaCharacterRepository.existsByCharacterName(characterName);
-    }
-
-    public Character findCharacterByCharacterName(String characterName){
-        return jpaCharacterRepository.getCharacterByCharacterName(characterName).get();
     }
 
 }
