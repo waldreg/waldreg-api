566e3d8ded80689f8a07f6f4bbfb7744da1a6450
seongha <11pi885@gmail.com>
2023-03-01 14:00:32 +0900

    feat : boardUserRepository 구현체 작성

---
+++ waldreg/repository-layer/board-jpa/src/main/java/org/waldreg/repository/board/BoardUserServiceProvider.java
@@ -0,0 +1,33 @@
+package org.waldreg.repository.board;
+
+import org.springframework.stereotype.Repository;
+import org.waldreg.board.board.spi.BoardUserRepository;
+import org.waldreg.board.dto.UserDto;
+import org.waldreg.domain.user.User;
+import org.waldreg.repository.board.mapper.BoardUserRepositoryMapper;
+import org.waldreg.repository.board.repository.JpaUserRepository;
+
+@Repository
+public class BoardUserServiceProvider implements BoardUserRepository{
+
+    private final JpaUserRepository jpaUserRepository;
+    private final BoardUserRepositoryMapper boardUserRepositoryMapper;
+
+    public BoardUserServiceProvider(JpaUserRepository jpaUserRepository, BoardUserRepositoryMapper boardUserRepositoryMapper){
+        this.jpaUserRepository = jpaUserRepository;
+        this.boardUserRepositoryMapper = boardUserRepositoryMapper;
+    }
+
+    @Override
+    public UserDto getUserInfo(int id){
+        User user = getUserById(id);
+        return boardUserRepositoryMapper.userToUserDto(user);
+    }
+
+    private User getUserById(int id){
+        return jpaUserRepository.findById(id).orElseThrow(
+                () -> {throw new IllegalStateException("Cannot find user id \"" + id + "\"");}
+        );
+    }
+
+}
