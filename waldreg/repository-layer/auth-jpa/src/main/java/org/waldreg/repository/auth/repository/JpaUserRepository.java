3090c8db63b12a91adc4172400f9a113e72c1dea
seongha <11pi885@gmail.com>
2023-03-03 13:18:22 +0900

    fix : authJpaUserRepository exist 수정

--- waldreg/repository-layer/auth-jpa/src/main/java/org/waldreg/repository/auth/repository/JpaUserRepository.java
+++ waldreg/repository-layer/auth-jpa/src/main/java/org/waldreg/repository/auth/repository/JpaUserRepository.java
@@ -10,8 +10,7 @@ import org.waldreg.domain.user.User;
 @Repository("authJpaUserRepository")
 public interface JpaUserRepository extends JpaRepository<User, Integer>{
 
-    @Query(value = "select EXISTS (select U.* from USER U where U.USER_USER_ID = :userId)", nativeQuery = true)
-    boolean isExistUserByUserId(@Param("userId") String userId);
+    boolean existsByUserInfoUserId(String userId);
 
     @Query(value = "select U.* from USER U where U.USER_USER_ID =:userId", nativeQuery = true)
     Optional<User> findUserByUserId(@Param("userId") String userId);
--- waldreg/repository-layer/board-jpa/src/main/java/org/waldreg/repository/board/repository/JpaCategoryRepository.java
+++ waldreg/repository-layer/board-jpa/src/main/java/org/waldreg/repository/board/repository/JpaCategoryRepository.java
@@ -1,8 +1,6 @@
 package org.waldreg.repository.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.waldreg.domain.board.category.Category;
 
