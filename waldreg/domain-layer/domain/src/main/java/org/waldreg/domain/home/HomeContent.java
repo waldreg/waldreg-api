7bc5eb3af45b0728180b1f7266c869897e4b9c12
devxb <devxb@naver.com>
2023-03-28 09:45:31 +0900

    feat : Home Service 초기세팅
    - Service Home-module 인터페이스 설정
    - Repositofy Jpa-Home-module 인터페이스 설정

---
+++ waldreg/domain-layer/domain/src/main/java/org/waldreg/domain/home/HomeContent.java
@@ -0,0 +1,68 @@
+package org.waldreg.domain.home;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "HOME_CONTENT")
+public final class HomeContent{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "HOME_CONTENT_ID")
+    private Integer id;
+
+    @Column(name = "HOME_CONTENT_CONTENT")
+    private String content;
+
+    private HomeContent(){}
+
+    public HomeContent(Builder builder){
+        this.id = builder.id;
+        this.content = builder.content;
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public static final class Builder{
+
+        private Integer id;
+        private String content;
+
+        private Builder(){}
+
+        public Builder id(Integer id){
+            this.id = id;
+            return this;
+        }
+
+        public Builder content(String content){
+            this.content = content;
+            return this;
+        }
+
+        public HomeContent build(){
+            return new HomeContent(this);
+        }
+
+    }
+
+}
