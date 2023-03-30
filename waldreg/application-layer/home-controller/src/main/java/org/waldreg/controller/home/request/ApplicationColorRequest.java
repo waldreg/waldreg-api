724694c914396d0d245b8ded7b510c882c965cfa
seongha <11pi885@gmail.com>
2023-03-30 17:33:17 +0900

    feat : 색상 hex코드 정규식 추가

--- waldreg/application-layer/home-controller/src/main/java/org/waldreg/controller/home/request/ApplicationColorRequest.java
+++ waldreg/application-layer/home-controller/src/main/java/org/waldreg/controller/home/request/ApplicationColorRequest.java
@@ -1,12 +1,15 @@
 package org.waldreg.controller.home.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.Pattern;
 
 public final class ApplicationColorRequest{
 
     @JsonProperty("primary_color")
+    @Pattern(regexp = "/#?([A-Fa-f0-9]){3}(([A-Fa-f0-9]){3})?/", message = "Invalid Color hex code \\”12345678910\\”")
     private String primaryColor;
     @JsonProperty("background_color")
+    @Pattern(regexp = "/#?([A-Fa-f0-9]){3}(([A-Fa-f0-9]){3})?/", message = "Invalid Color hex code \\”12345678910\\”")
     private String backgroundColor;
 
     public ApplicationColorRequest(){}
